df07eeafc71f1ab332e92645609036a013758885
Jaka Maver <jm18457@gmail.com>
2024-12-01 08:48:53 +0100

    feat: Add template

---
+++ template/index.mjs
@@ -0,0 +1,18 @@
+import fs from "node:fs";
+
+function readInput() {
+  const input = process.env.INPUT ?? "sample.txt";
+  return fs.readFileSync(new URL(input, import.meta.url), "utf-8");
+}
+
+function parseInput() {}
+
+function solvePart1() {}
+
+function solvePart2() {}
+
+// Run code
+const part1Result = solvePart1();
+console.log(`Part 1: ${part1Result}`);
+const part2Result = solvePart2();
+console.log(`Part 2: ${part2Result}`);
