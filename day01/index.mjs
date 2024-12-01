c7518cbe7ff455f7bfa2009e9a9237afc8325d3e
Jaka Maver <jm18457@gmail.com>
2024-12-01 08:44:58 +0100

    feat: Refactor functions

--- day01/index.mjs
+++ day01/index.mjs
@@ -1,40 +1,49 @@
-import fs from "node:fs/promises";
-import assert from "node:assert/strict";
+import fs from "node:fs";
 
-// Parse input
-const rawData = await fs.readFile(
-  new URL("input.txt", import.meta.url),
-  "utf-8",
-);
-const { leftList, rightList } = rawData.split("\n").reduce(
-  ({ leftList, rightList }, line) => {
-    const match = line.match(/(\d+)\s+(\d+)/);
-    return {
-      leftList: leftList.concat(parseInt(match[1])),
-      rightList: rightList.concat(parseInt(match[2])),
-    };
-  },
-  { leftList: [], rightList: [] },
-);
-assert.equal(leftList.length, rightList.length);
-
-// Part 1 solution
-leftList.sort((a, b) => a - b);
-rightList.sort((a, b) => a - b);
-let distance = 0;
-for (let i = 0; i < leftList.length; i++) {
-  distance += Math.abs(leftList[i] - rightList[i]);
+// Parse input into 2 separate lists of numbers
+function parseInput() {
+  const input = process.env.INPUT ?? "sample.txt";
+  const rawData = fs.readFileSync(new URL(input, import.meta.url), "utf-8");
+  const { leftList, rightList } = rawData.split("\n").reduce(
+    ({ leftList, rightList }, line) => {
+      const match = line.match(/(\d+)\s+(\d+)/);
+      return {
+        leftList: leftList.concat(parseInt(match[1])),
+        rightList: rightList.concat(parseInt(match[2])),
+      };
+    },
+    { leftList: [], rightList: [] },
+  );
+  return { leftList, rightList };
 }
-console.log(`Part 1: ${distance}`);
 
-// Part 2 solution
-const rightListNumberToCountMap = new Map();
-rightList.forEach((number) => {
-  const count = rightListNumberToCountMap.get(number) ?? 0;
-  rightListNumberToCountMap.set(number, count + 1);
-});
-let similarityScore = 0;
-leftList.forEach((number) => {
-  similarityScore += number * (rightListNumberToCountMap.get(number) ?? 0);
-});
-console.log(`Part 2: ${similarityScore}`);
+function solvePart1() {
+  const { leftList, rightList } = parseInput();
+  leftList.sort((a, b) => a - b);
+  rightList.sort((a, b) => a - b);
+  let distance = 0;
+  for (let i = 0; i < leftList.length; i++) {
+    distance += Math.abs(leftList[i] - rightList[i]);
+  }
+  return distance;
+}
+
+function solvePart2() {
+  const { leftList, rightList } = parseInput();
+  const frequencyMap = new Map();
+  rightList.forEach((number) => {
+    const count = frequencyMap.get(number) ?? 0;
+    frequencyMap.set(number, count + 1);
+  });
+  let similarityScore = 0;
+  leftList.forEach((number) => {
+    similarityScore += number * (frequencyMap.get(number) ?? 0);
+  });
+  return similarityScore;
+}
+
+// Run code
+const part1Result = solvePart1();
+console.log(`Part 1: ${part1Result}`);
+const part2Result = solvePart2();
+console.log(`Part 2: ${part2Result}`);
