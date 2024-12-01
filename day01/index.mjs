5d379fc8e26266b5147ffab97bcd4f07ae04dd5a
Jaka Maver <jm18457@gmail.com>
2024-12-01 08:15:50 +0100

    feat: Solve part 2

--- day01/index.mjs
+++ day01/index.mjs
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises';
 
+// Parse input
 const rawData = await fs.readFile(new URL('input.txt', import.meta.url), 'utf-8');
 const rawLines = rawData.split('\n')
 const lines = rawLines.map(line => {
@@ -8,13 +9,28 @@ const lines = rawLines.map(line => {
 })
 const leftList = lines.map(([first, second]) => first)
 const rightList = lines.map(([first, second]) => second)
+
+
+// Part 1 solution
 leftList.sort((a, b) => a - b)
 rightList.sort((a, b) => a - b)
-
 let distance = 0;
 for (let i = 0; i < leftList.length; i++) {
     const left = leftList[i]
     const right = rightList[i]
     distance += Math.abs(left - right)
 }
-console.log(distance)
+console.log(`Part 1: ${distance}`)
+
+
+// Part 2 solution
+const rightListNumberToCountMap = new Map()
+rightList.forEach(number => {
+    const count = rightListNumberToCountMap.get(number) ?? 0
+    rightListNumberToCountMap.set(number, count + 1)
+})
+let similarityScore = 0;
+leftList.forEach(number => {
+    similarityScore += number * (rightListNumberToCountMap.get(number) ?? 0)
+})
+console.log(`Part 2: ${similarityScore}`)
