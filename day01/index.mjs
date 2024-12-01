376221252bdfc0b9de50e2c7b51227f9766d4322
Jaka Maver <jm18457@gmail.com>
2024-12-01 08:23:41 +0100

    feat: Install prettier

--- day01/index.mjs
+++ day01/index.mjs
@@ -1,38 +1,42 @@
-import fs from 'node:fs/promises';
-import assert from 'node:assert/strict';
+import fs from "node:fs/promises";
+import assert from "node:assert/strict";
 
 // Parse input
-const rawData = await fs.readFile(new URL('input.txt', import.meta.url), 'utf-8');
-const {leftList, rightList} = rawData.split('\n').reduce(({leftList, rightList}, line) => {
-    const match = line.match(/(\d+)\s+(\d+)/)
+const rawData = await fs.readFile(
+  new URL("input.txt", import.meta.url),
+  "utf-8",
+);
+const { leftList, rightList } = rawData.split("\n").reduce(
+  ({ leftList, rightList }, line) => {
+    const match = line.match(/(\d+)\s+(\d+)/);
     return {
-        leftList: leftList.concat(parseInt(match[1])),
-        rightList: rightList.concat(parseInt(match[2])),
-    }
-}, { leftList: [], rightList: []})
-assert.equal(leftList.length, rightList.length)
-
+      leftList: leftList.concat(parseInt(match[1])),
+      rightList: rightList.concat(parseInt(match[2])),
+    };
+  },
+  { leftList: [], rightList: [] },
+);
+assert.equal(leftList.length, rightList.length);
 
 // Part 1 solution
-leftList.sort((a, b) => a - b)
-rightList.sort((a, b) => a - b)
+leftList.sort((a, b) => a - b);
+rightList.sort((a, b) => a - b);
 let distance = 0;
 for (let i = 0; i < leftList.length; i++) {
-    const left = leftList[i]
-    const right = rightList[i]
-    distance += Math.abs(left - right)
+  const left = leftList[i];
+  const right = rightList[i];
+  distance += Math.abs(left - right);
 }
-console.log(`Part 1: ${distance}`)
-
+console.log(`Part 1: ${distance}`);
 
 // Part 2 solution
-const rightListNumberToCountMap = new Map()
-rightList.forEach(number => {
-    const count = rightListNumberToCountMap.get(number) ?? 0
-    rightListNumberToCountMap.set(number, count + 1)
-})
+const rightListNumberToCountMap = new Map();
+rightList.forEach((number) => {
+  const count = rightListNumberToCountMap.get(number) ?? 0;
+  rightListNumberToCountMap.set(number, count + 1);
+});
 let similarityScore = 0;
-leftList.forEach(number => {
-    similarityScore += number * (rightListNumberToCountMap.get(number) ?? 0)
-})
-console.log(`Part 2: ${similarityScore}`)
+leftList.forEach((number) => {
+  similarityScore += number * (rightListNumberToCountMap.get(number) ?? 0);
+});
+console.log(`Part 2: ${similarityScore}`);
