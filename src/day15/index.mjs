16c2e69d75d324e13e1ace3e8e607dd99f62dae6
Jaka Maver <jm18457@gmail.com>
2024-12-07 05:51:46 +0100

    feat: Add all files/folders

---
+++ src/day15/index.mjs
@@ -0,0 +1,27 @@
+import fs from "node:fs";
+
+function readInput() {
+  const input = process.env.INPUT ?? "sample.txt";
+  return fs.readFileSync(new URL(input, import.meta.url), "utf-8");
+}
+
+function parseInput() {
+  const data = readInput();
+  return data;
+}
+
+function solvePart1() {
+  const input = parseInput();
+  return;
+}
+
+function solvePart2() {
+  const input = parseInput();
+  return;
+}
+
+// Run code
+const part1Result = solvePart1();
+console.log(`Part 1: ${part1Result}`);
+const part2Result = solvePart2();
+console.log(`Part 2: ${part2Result}`);
