a119fae224a2930911cae341de92cbec5ebda391
Jaka Maver <jm18457@gmail.com>
2024-12-11 07:26:48 +0100

    feat(day11): Solve
    Squashed commit of the following:
    
    commit 2972211d33c123def130c7b2c1c943052014e693
    Author: Jaka Maver <jm18457@gmail.com>
    Date:   Wed Dec 11 07:26:36 2024 +0100
    
        solve p2
    
    commit e11bc63e346b7cc0386b7dc55ac02b8c45d288ad
    Author: Jaka Maver <jm18457@gmail.com>
    Date:   Wed Dec 11 07:04:46 2024 +0100
    
        solve p1
    
    commit 562432aff6a17183dc41455934484fbbc0b25e2d
    Author: Jaka Maver <jm18457@gmail.com>
    Date:   Wed Dec 11 06:50:03 2024 +0100
    
        add parsing

--- src/day11/index.mjs
+++ src/day11/index.mjs
@@ -7,17 +7,74 @@ function readInput() {
 
 function parseInput() {
   const data = readInput();
-  return data;
+  return data.split(" ").map(Number);
+}
+
+/**
+ * Changes a stone based on criteria provided
+ *
+ * @param {number} stone
+ * @returns {number[]} - stones
+ */
+function change(stone) {
+  if (stone === 0) {
+    return [1];
+  }
+
+  const string = stone.toString();
+  if (string.length % 2 === 0) {
+    return [
+      parseInt(string.substring(0, string.length / 2)),
+      parseInt(string.substring(string.length / 2)),
+    ];
+  }
+
+  return [stone * 2024];
 }
 
 function solvePart1() {
-  const input = parseInput();
-  return;
+  let stones = parseInput();
+  // Brute force work for part 1
+  for (let i = 0; i < 25; i++) {
+    stones = stones.flatMap((stone) => change(stone));
+  }
+  return stones.length;
+}
+
+/**
+ * Returns memoization key given a stone and a remainder
+ *
+ * @param {number} stone
+ * @param {number} remainder
+ * @returns {string}
+ */
+function getKey(stone, remainder) {
+  return `${stone}::${remainder}`;
+}
+
+function solve(stone, remainder, cache) {
+  if (remainder === 0) {
+    return 1;
+  }
+
+  const key = getKey(stone, remainder);
+  if (cache.has(key)) {
+    return cache.get(key);
+  }
+
+  const result = change(stone)
+    .map((stone) => solve(stone, remainder - 1, cache))
+    .reduce((sum, length) => length + sum, 0);
+  cache.set(key, result);
+  return result;
 }
 
 function solvePart2() {
-  const input = parseInput();
-  return;
+  const stones = parseInput();
+  const cache = new Map();
+  return stones
+    .map((stone) => solve(stone, 75, cache))
+    .reduce((sum, length) => sum + length, 0);
 }
 
 // Run code
