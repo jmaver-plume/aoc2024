8d25b97c5637d6a4584f7bfc842b6a1024095ae3
Jaka Maver <jm18457@gmail.com>
2024-12-14 10:11:41 +0100

    feat(day14): Solve p2 (requires viewing)

--- src/day14/index.mjs
+++ src/day14/index.mjs
@@ -5,11 +5,39 @@ function readInput() {
   return fs.readFileSync(new URL(input, import.meta.url), "utf-8");
 }
 
+const MAX_X = 101;
+const MAX_Y = 103;
+
+function print(positions) {
+  const grid = [];
+  for (let i = 0; i < MAX_Y; i++) {
+    const row = [];
+    for (let j = 0; j < MAX_X; j++) {
+      row.push(".");
+    }
+    grid.push(row);
+  }
+
+  positions.forEach((position) => {
+    const value = grid[position.y][position.x];
+    if (value === ".") {
+      grid[position.y][position.x] = 1;
+    } else {
+      grid[position.y][position.x] = value + 1;
+    }
+  });
+
+  grid.forEach((row) => {
+    console.log(row.join(""));
+  });
+}
+
 function parseInput() {
   const data = readInput();
-  return data.split("\n").map((line) => {
+  return data.split("\n").map((line, i) => {
     const matches = [...line.matchAll(/([+-]?\d+)/g)];
     return {
+      id: i,
       p: {
         x: parseInt(matches[0][0]),
         y: parseInt(matches[1][0]),
@@ -24,20 +52,15 @@ function parseInput() {
 
 function solvePart1() {
   const robots = parseInput();
-  const maxX = 101;
-  // const maxX = 11;
-  const maxY = 103;
-  // const maxY = 7;
-  const midX = (maxX - 1) / 2;
-  const midY = (maxY - 1) / 2;
+  const midX = (MAX_X - 1) / 2;
+  const midY = (MAX_Y - 1) / 2;
   const positions = robots
     .map((robot) => {
-      const x = (robot.p.x + 100 * robot.v.x) % maxX;
-      const y = (robot.p.y + 100 * robot.v.y) % maxY;
-      return { x: x < 0 ? maxX + x : x, y: y < 0 ? maxY + y : y };
+      const x = (robot.p.x + 100 * robot.v.x) % MAX_X;
+      const y = (robot.p.y + 100 * robot.v.y) % MAX_Y;
+      return { x: x < 0 ? MAX_X + x : x, y: y < 0 ? MAX_Y + y : y };
     })
     .filter((position) => position.x !== midX && position.y !== midY);
-
   return Object.values(
     Object.groupBy(positions, ({ x, y }) => {
       if (x < midX && y < midY) {
@@ -60,9 +83,62 @@ function solvePart1() {
     .reduce((acc, val) => acc * val, 1);
 }
 
+function walk(robot) {
+  const x = (robot.p.x + robot.v.x) % MAX_X;
+  const y = (robot.p.y + robot.v.y) % MAX_Y;
+  robot.p.x = x < 0 ? MAX_X + x : x;
+  robot.p.y = y < 0 ? MAX_Y + y : y;
+}
+
 function solvePart2() {
+  // When running with a different output you need to adjust these 2 values as they are input dependent
+  const MIDDLE_X = 53;
+  const MIDDLE_X_COUNT = 27;
+
+  const grid = [];
+  for (let i = 0; i < MAX_Y; i++) {
+    const row = [];
+    for (let j = 0; j < MAX_X; j++) {
+      row.push(".");
+    }
+    grid.push(row);
+  }
+
+  const counts = [];
   const robots = parseInput();
-  return;
+  for (let i = 0; i <= 10403; i++) {
+    robots.forEach((robot) => {
+      // unset previous
+      grid[robot.p.y][robot.p.x] = ".";
+
+      walk(robot);
+
+      // set next/current
+      grid[robot.p.y][robot.p.x] = "X";
+    });
+
+    let count = 0;
+    for (let y = 0; y < MAX_Y; y++) {
+      if (grid[y][MIDDLE_X] === "X") {
+        count += 1;
+      }
+    }
+
+    counts.push({ i, count });
+
+    // I first logged sorted counts to find the top counts then I printer the grid
+    if (count >= MIDDLE_X_COUNT) {
+      console.log(`### Start of ${i} ###`);
+      print(robots.map((robot) => robot.p));
+      console.log(`### End of ${i} ###`);
+      console.log();
+    }
+  }
+
+  console.log(
+    "Counts:",
+    counts.sort((a, b) => b.count - a.count),
+  );
 }
 
 // Run code
