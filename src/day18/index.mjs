e50cbab3398d52c16fcb7e1dbab92712dbe4c081
Jaka Maver <jm18457@gmail.com>
2024-12-20 18:03:35 +0100

    feat(day18): solve sample

--- src/day18/index.mjs
+++ src/day18/index.mjs
@@ -1,5 +1,13 @@
 import fs from "node:fs";
-import {createEmptyGrid, printGrid} from "../util.mjs";
+import {
+  createEmptyGrid,
+  getGridNeighbours,
+  makeGridIterator,
+  positionToString,
+  printGrid,
+  PriorityQueue,
+  stringToPosition,
+} from "../util.mjs";
 
 function readInput() {
   const input = process.env.INPUT ?? "sample.txt";
@@ -17,11 +25,46 @@ function parseInput() {
 function solvePart1() {
   const bytes = parseInput();
   const grid = createEmptyGrid(7, 7, ".");
-  bytes.slice(0, 12).forEach(byte => {
-    grid[byte.y][byte.x] = '#'
-  })
-  printGrid(grid)
-  return;
+  bytes.slice(0, 12).forEach((byte) => {
+    grid[byte.y][byte.x] = "#";
+  });
+
+  // Solve using Dijkstra
+  const pq = new PriorityQueue();
+  const dist = {};
+  const prev = {};
+
+  // Assign distance to every node
+  for (const { x, y, value } of makeGridIterator(grid)) {
+    if (value === "#") {
+      continue;
+    }
+    const key = positionToString({ x, y });
+    const distance = x === 0 && y === 0 ? 0 : Infinity;
+    dist[key] = distance;
+    pq.add(key, distance);
+  }
+
+  while (!pq.isEmpty()) {
+    const u = pq.extractMin();
+    const position = stringToPosition(u);
+    const neighbours = getGridNeighbours(position, grid)
+      .filter((neighbour) => {
+        return grid[neighbour.y][neighbour.x] !== "#";
+      })
+      .map((neighbour) => positionToString(neighbour));
+    neighbours.forEach((neighbour) => {
+      const alt = dist[u] + 1;
+      if (alt < dist[neighbour]) {
+        prev[neighbour] = u;
+        dist[neighbour] = alt;
+        pq.decreasePriority(neighbour, alt);
+      }
+    });
+  }
+
+  const end = positionToString({ x: 6, y: 6 });
+  return dist[end];
 }
 
 function solvePart2() {
