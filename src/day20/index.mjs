f5ab1bf3c04c1705dae61dcef41445ee79f6f107
Jaka Maver <jm18457@gmail.com>
2024-12-20 20:53:34 +0100

    feat(day20): refactor

--- src/day20/index.mjs
+++ src/day20/index.mjs
@@ -21,8 +21,7 @@ function parseInput() {
   return parseGridInput(data);
 }
 
-function solvePart1() {
-  const grid = parseInput();
+function findPath(grid) {
   const start = findUniqueInGrid((value) => value === "S", grid);
   const end = findUniqueInGrid((value) => value === "E", grid);
   const path = [positionToString(start)];
@@ -32,20 +31,25 @@ function solvePart1() {
     const next = getGridNeighbours(current, grid).find(
       (p) => grid[p.y][p.x] !== "#" && !visited.has(positionToString(p)),
     );
-    path.push(positionToString(next));
+    path.push(next);
     current = next;
     visited.add(positionToString(next));
   }
+  return path;
+}
+
+function solvePart1() {
+  const grid = parseInput();
+  const path = findPath(grid);
 
   const locationToIndex = {};
-  path.forEach((location, index) => {
-    locationToIndex[location] = index;
+  path.forEach((position, index) => {
+    locationToIndex[positionToString(position)] = index;
   });
 
   const cheats = [];
-  path.forEach((location) => {
-    const index = locationToIndex[location];
-    const position = stringToPosition(location);
+  path.forEach((position) => {
+    const index = locationToIndex[positionToString(position)];
     const candidates = [];
     if (position.x < grid[0].length - 2) {
       candidates.push({
@@ -89,8 +93,57 @@ function solvePart1() {
 }
 
 function solvePart2() {
-  const input = parseInput();
-  return;
+  const grid = parseInput();
+  const path = findPath(grid);
+
+  const locationToIndex = {};
+  path.forEach((position, index) => {
+    locationToIndex[positionToString(position)] = index;
+  });
+
+  const cheats = [];
+  path.forEach((position) => {
+    const index = locationToIndex[positionToString(position)];
+    const candidates = [];
+    if (position.x < grid[0].length - 2) {
+      candidates.push({
+        position,
+        nextPosition: { x: position.x + 1, y: position.y },
+        nextNextPosition: { x: position.x + 2, y: position.y },
+      });
+    }
+    if (position.x > 1) {
+      candidates.push({
+        position,
+        nextPosition: { x: position.x - 1, y: position.y },
+        nextNextPosition: { x: position.x - 2, y: position.y },
+      });
+    }
+    if (position.y < grid.length - 2) {
+      candidates.push({
+        position,
+        nextPosition: { x: position.x, y: position.y + 1 },
+        nextNextPosition: { x: position.x, y: position.y + 2 },
+      });
+    }
+    if (position.y > 1) {
+      candidates.push({
+        position,
+        nextPosition: { x: position.x, y: position.y - 1 },
+        nextNextPosition: { x: position.x, y: position.y - 2 },
+      });
+    }
+    candidates.forEach((candidate) => {
+      const { nextPosition, nextNextPosition } = candidate;
+      const next = grid[nextPosition.y][nextPosition.x];
+      const nextNext = grid[nextNextPosition.y][nextNextPosition.x];
+      const cheatIndex = locationToIndex[positionToString(nextNextPosition)];
+      if (next === "#" && nextNext === "." && index < cheatIndex) {
+        cheats.push(cheatIndex - index - 2);
+      }
+    });
+  });
+  return cheats.filter((cheat) => cheat >= 100).length;
 }
 
 // Run code
