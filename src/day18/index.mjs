70b67d06515efd6df5b11bbd10efc1f6ac052269
Jaka Maver <jm18457@gmail.com>
2024-12-20 18:11:41 +0100

    feat(day18): solve p2

--- src/day18/index.mjs
+++ src/day18/index.mjs
@@ -79,8 +79,57 @@ function solvePart1() {
 }
 
 function solvePart2() {
-  const input = parseInput();
-  return;
+  const size = isSample() ? 7 : 71;
+  const minDuration = isSample() ? 12 : 1024;
+
+  const bytes = parseInput();
+  const grid = createEmptyGrid(size, size, ".");
+  for (let i = 0; i < bytes.length; i++) {
+    const byte = bytes[i];
+    grid[byte.y][byte.x] = "#";
+    if (i <= minDuration) {
+      continue;
+    }
+
+    // Solve using Dijkstra
+    const pq = new PriorityQueue();
+    const dist = {};
+    const prev = {};
+
+    // Assign distance to every node
+    for (const { x, y, value } of makeGridIterator(grid)) {
+      if (value === "#") {
+        continue;
+      }
+      const key = positionToString({ x, y });
+      const distance = x === 0 && y === 0 ? 0 : Infinity;
+      dist[key] = distance;
+      pq.add(key, distance);
+    }
+
+    while (!pq.isEmpty()) {
+      const u = pq.extractMin();
+      const position = stringToPosition(u);
+      const neighbours = getGridNeighbours(position, grid)
+        .filter((neighbour) => {
+          return grid[neighbour.y][neighbour.x] !== "#";
+        })
+        .map((neighbour) => positionToString(neighbour));
+      neighbours.forEach((neighbour) => {
+        const alt = dist[u] + 1;
+        if (alt < dist[neighbour]) {
+          prev[neighbour] = u;
+          dist[neighbour] = alt;
+          pq.decreasePriority(neighbour, alt);
+        }
+      });
+    }
+
+    const end = positionToString({ x: size - 1, y: size - 1 });
+    if (dist[end] === Infinity) {
+      return `${bytes[i].x},${bytes[i].y}`;
+    }
+  }
 }
 
 // Run code
