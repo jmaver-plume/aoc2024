5c22c9a49b229adfca664b89be0e6ad47686d404
Jaka Maver <jm18457@gmail.com>
2024-12-23 07:18:38 +0100

    feat(day23): solve

--- src/day23/index.mjs
+++ src/day23/index.mjs
@@ -1,4 +1,6 @@
 import fs from "node:fs";
+import { Edge, Graph, Node, toDot } from "ts-graphviz";
+import clipboard from "clipboardy";
 
 function readInput() {
   const input = process.env.INPUT ?? "sample.txt";
@@ -7,17 +9,79 @@ function readInput() {
 
 function parseInput() {
   const data = readInput();
-  return data;
+  return data.split("\n").map((line) => line.split("-"));
+}
+
+function createGraphFromInput(input) {
+  const graph = {};
+  input.forEach((edge) => {
+    const [left, right] = edge;
+    if (!graph[left]) {
+      graph[left] = new Set();
+    }
+    if (!graph[right]) {
+      graph[right] = new Set();
+    }
+    graph[left].add(right);
+    graph[right].add(left);
+  });
+  return graph;
 }
 
 function solvePart1() {
   const input = parseInput();
-  return;
+  const graph = createGraphFromInput(input);
+
+  const results = new Set();
+  Object.entries(graph).forEach((entry) => {
+    const [node, edges] = entry;
+    const edgesArr = Array.from(edges);
+    const combinations = [];
+    for (let i = 0; i < edgesArr.length; i++) {
+      for (let j = i + 1; j < edgesArr.length; j++) {
+        combinations.push([edgesArr[i], edgesArr[j]]);
+      }
+    }
+    combinations.forEach((combination) => {
+      const [left, right] = combination;
+      if (graph[left].has(right)) {
+        const list = [node, left, right].sort();
+        if (list.some((item) => item.startsWith("t"))) {
+          results.add(list.join(","));
+        }
+      }
+    });
+  });
+
+  return results.size;
 }
 
 function solvePart2() {
   const input = parseInput();
-  return;
+  const graph = createGraphFromInput(input);
+
+  // Greedy algorithm for finding maximal clique:
+  //   - https://en.wikipedia.org/wiki/Clique_problem#Finding_a_single_maximal_clique
+  const cliques = {};
+  Object.entries(graph).forEach((entry) => {
+    const [node, edges] = entry;
+    const clique = new Set([node]);
+    edges.forEach((edge) => {
+      if (Array.from(clique).every((item) => graph[item].has(edge))) {
+        clique.add(edge);
+      }
+    });
+    cliques[node] = clique;
+  });
+
+  let maxClique;
+  for (const key in cliques) {
+    const clique = cliques[key];
+    if (!maxClique || maxClique.size < clique.size) {
+      maxClique = clique;
+    }
+  }
+  return Array.from(maxClique).sort().join(",");
 }
 
 // Run code
