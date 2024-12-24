a0cac12930dc650c117cc10a40009d4c86dfdbf7
Jaka Maver <jm18457@gmail.com>
2024-12-24 06:52:33 +0100

    feat(day24): solve p1

--- src/day24/index.mjs
+++ src/day24/index.mjs
@@ -7,12 +7,71 @@ function readInput() {
 
 function parseInput() {
   const data = readInput();
-  return data;
+  const [rawValues, rawConnections] = data.split("\n\n");
+  const values = rawValues
+    .split("\n")
+    .map((line) => {
+      return { key: line.split(": ")[0], value: parseInt(line.split(": ")[1]) };
+    })
+    .reduce((acc, value) => {
+      acc[value.key] = value.value;
+      return acc;
+    }, {});
+  const connections = rawConnections.split("\n").map((line) => {
+    const match = line.match(
+      /([a-zA-Z0-9]+) (AND|XOR|OR) ([a-zA-Z0-9]+) -> ([a-zA-Z0-9]+)/,
+    );
+    return {
+      left: match[1],
+      operator: match[2],
+      right: match[3],
+      result: match[4],
+    };
+  });
+  return { values, connections };
 }
 
 function solvePart1() {
   const input = parseInput();
-  return;
+  const unset = new Set();
+  input.connections.forEach((connection) => {
+    if (!(connection.left in input.values)) {
+      unset.add(connection.left);
+    }
+    if (!(connection.left in input.values)) {
+      unset.add(connection.right);
+    }
+  });
+
+  const values = input.values;
+  let connections = input.connections;
+  while (connections.length) {
+    const remainingConnections = [];
+    connections.forEach((connection) => {
+      if (!(connection.left in values) || !(connection.right in values)) {
+        remainingConnections.push(connection);
+        return;
+      }
+      if (connection.operator === "AND") {
+        values[connection.result] =
+          values[connection.left] & values[connection.right];
+      } else if (connection.operator === "OR") {
+        values[connection.result] =
+          values[connection.left] | values[connection.right];
+      } else if (connection.operator === "XOR") {
+        values[connection.result] =
+          values[connection.left] ^ values[connection.right];
+      }
+    });
+    connections = remainingConnections;
+  }
+
+  return parseInt(Object.entries(values)
+    .filter((entry) => entry[0].startsWith("z"))
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .reverse()
+    .map((entry) => entry[1])
+    .join(""), 2);
 }
 
 function solvePart2() {
