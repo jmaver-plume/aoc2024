880b14816928ab713f61fa122d5ccdd59637e484
Jaka Maver <jm18457@gmail.com>
2024-12-26 07:44:00 +0100

    feat(day25): solve

--- src/day25/index.mjs
+++ src/day25/index.mjs
@@ -7,12 +7,76 @@ function readInput() {
 
 function parseInput() {
   const data = readInput();
-  return data;
+  const schematics = data
+    .split("\n\n")
+    .map((schematic) => schematic.split("\n").map((line) => line.split("")));
+  const keys = schematics
+    .filter((schematic) => {
+      return (
+        schematic.at(0).every((item) => item === ".") &&
+        schematic.at(-1).every((item) => item === "#")
+      );
+    })
+    .map((key) => {
+      const heights = [];
+      for (let x = 0; x < key[0].length; x++) {
+        let height = 0;
+        for (let y = key.length - 1; y >= 0; y--) {
+          const value = key[y][x];
+          if (value === "#") {
+            height += 1;
+          } else if (value === ".") {
+            break;
+          }
+        }
+        heights.push(height);
+      }
+      return heights;
+    });
+  const locks = schematics
+    .filter((schematic) => {
+      return (
+        schematic.at(0).every((item) => item === "#") &&
+        schematic.at(-1).every((item) => item === ".")
+      );
+    })
+    .map((lock) => {
+      const heights = [];
+      for (let x = 0; x < lock[0].length; x++) {
+        let height = 0;
+        for (let y = 0; y < lock.length; y++) {
+          const value = lock[y][x];
+          if (value === "#") {
+            height += 1;
+          } else if (value === ".") {
+            break;
+          }
+        }
+        heights.push(height);
+      }
+      return heights;
+    });
+  return { keys, locks };
 }
 
 function solvePart1() {
-  const input = parseInput();
-  return;
+  const { keys, locks } = parseInput();
+  let count = 0
+  keys.forEach(key => {
+    locks.forEach(lock => {
+      let matches = true
+      for (let i = 0; i < key.length; i++) {
+        if (key[i] + lock[i] > 7) {
+          matches = false;
+          break;
+        }
+      }
+      if (matches) {
+        count += 1
+      }
+    })
+  })
+  return count;
 }
 
 function solvePart2() {
